ea9d1aec92011c704e78005a664b25a442d1fd10
Bug-001 <191850169@smail.nju.edu.cn>
2021-08-17 00:56:46 +0800

    Update: add comments

--- geneticAlg/algorithm.py
+++ geneticAlg/algorithm.py
@@ -36,19 +36,27 @@ def mutate(ch):
     return np.where(np.random.rand(n) < RATE_MUTATE, np.random.randint(m), ch)
 
 def geneticAlg():
+    # init chromosome
     chromosome = initChromosome()
     for i in tqdm(range(NR_ITERATION)):
+        # calculate the elapsed time corresponding to the certain chromosome
         ans = np.apply_along_axis(calAnswer, axis=1, arr=chromosome)
+        # drawing
         plt.scatter(i*np.ones(NR_CHROMOSOME), ans, s=1, color='b')
+        # calculate adaptation
         adaption = calAdaptation(ans)
+        # save excellent chromosome
         exRank = int(NR_CHROMOSOME * RATE_EXCELLENT)
         nextGeneration = chromosome[np.argsort(-adaption)[:exRank]]
         selectRate = adaption / np.sum(adaption)
         for i in range(NR_CHROMOSOME - exRank):
+            # choose, cross, and mutate
             ch1, ch2 = chromosome[np.random.choice(np.arange(0, NR_CHROMOSOME), size=2, replace=False, p=selectRate)]
             newchrome = mutate(cross(ch1, ch2))
             nextGeneration = np.r_[nextGeneration, [newchrome]]
+        # update chromosome of new generation
         chromosome = nextGeneration
+    # draw converging scatter plot and return optimized slolution
     ret = chromosome[np.argmin(np.apply_along_axis(calAnswer, axis=1, arr=chromosome))]
     print(calAnswer(ret))
     plt.show()
@@ -56,14 +64,12 @@ def geneticAlg():
     
 
 if __name__ == "__main__":
+    # performance data of m servers
     servers = np.load('mDat.npy')
+    # time of n tasks to be done
     tasks = np.load('nDat.npy')
     m = servers.shape[0]
     n = tasks.shape[0]
-    
-    
-
+    # return an optimized solution
     ans = geneticAlg()
     np.save('ans.npy', ans)
-
-
